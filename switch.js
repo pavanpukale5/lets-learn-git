04705f683f6ae65c9b2ce803387cf14371fec527
pavanpukale <pavan.pukale@blu-ocean.net>
2022-07-03 20:09:03 +0530

    first commit

---
+++ switch.js
@@ -0,0 +1,27 @@
+// let targetId = "btnDelete";
+// switch (targetId){
+//     case "btnUpdate":
+//         console.log("btnUpdate");
+//         break;
+//     case "btnDelete":
+//         console.log("btnDelete");
+//         break;
+//     case "btnNew":
+//         console.log("btnNew");
+//         break;
+// }
+function targetId(a) {
+    switch (a) {
+        case "btnUpdate":
+            console.log("btnUpdate");
+            break;
+        case "btnDelete":
+            console.log("btnDelete");
+            break;
+        case "btnNew":
+            console.log("btnNew");
+            break;
+    }
+}
+targetId("btnUpdate");
+targetId("btnNew");
